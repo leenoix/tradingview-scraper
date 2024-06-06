6ced1b4e1dfd8eb1b8f9146de942edc0e590aad1
leenoix <jooyoung@hemeko.com>
2024-06-06 17:43:35 +0900

    Fix

--- src/TradingViewWebSocket.ts
+++ src/TradingViewWebSocket.ts
@@ -28,7 +28,7 @@ export class TradingViewWebSocket extends (EventEmitter as new () => TypedEmitte
       origin: 'https://data.tradingview.com'
     });
     this.ws.on('message', message => this.wsOnMessage(message.toString()));
-    this.ws.on('error', message => console.log('tradingview websocket error:', e));
+    this.ws.on('error', e => console.log('tradingview websocket error:', e));
     await this.tvSessionReady();
   }
 
